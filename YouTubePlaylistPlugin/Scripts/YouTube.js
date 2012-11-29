b5086cf7fcb33a9f39503ed6af2c9f9477e7045e
Chris <hockey73god@yahoo.com>
2012-11-28 23:27:59 -0500

    Cleanup
    Adding comments and misc functionality

--- YouTubePlaylistPlugin/Scripts/YouTube.js
+++ YouTubePlaylistPlugin/Scripts/YouTube.js
@@ -1,10 +1,5 @@
-﻿/*
-TODO:   Add BuildPager function to introduce true paging (eg list of pages (1, 2, 3, etc))
-*/
-
+﻿// Create global variables
 var displayRatings;
-
-// Create global variables so we aren't passing these values over and over???
 var user;
 var numberOfPlaylists;
 var videosPerPage;
@@ -15,10 +10,13 @@ var showDescription;
 var titleCharacters = 0;
 var showViews;
 var hdOnly;
+var showList;
 
 $(document).ready(function () {
 
+    // These events need to be 'on' because they are loaded after the DOM is ready
     $('body').on('click', '.fancy_video', function (ev) {
+        // Call fancybox on the video
         $.fancybox({
             href: this.href,
             type: 'iframe'
@@ -27,12 +25,14 @@ $(document).ready(function () {
     });
 
     $('#playlists').on('click', 'span', function () {
+        // The user has clicked on a playlist, get the videos for that playlist
         currentPlaylistId = $(this).attr('id');
         videosInCurrentPlaylist = $(this).attr('data-value');
         GetVideos(1);
     });
 
     $('#videos').on('click', 'span', function () {
+        // This event is fired when the user interacts with the pager
         GetVideos($(this).attr('start-index'));
     });
 });
@@ -49,38 +49,58 @@ function GetPlaylists(username) {
     $.getJSON(jsonURL, function (data) {
         var playlistIDs = '';
         playlistIDs += '<ul>';
+
         $.each(data.feed.entry, function (i, item) {
             var newPlaylist = new Playlist(item);
             if (newPlaylist.numberOfVideos > 0)
                 playlistIDs += newPlaylist.playlistBlock();
         });
+
         playlistIDs += '</ul>';
         $(playlistIDs).appendTo('#playlists');
     });
 }
 
 function GetVideos(p_startIndex) {
-    $('#videos').html('');
     var feedURL = 'http://gdata.youtube.com/feeds/api/playlists/';
     var playListURL = feedURL + currentPlaylistId + '?v=2&alt=json&callback=?';
 
+    // Clear the videos div
+    $('#videos').html('');
+
+    // Used for the paging functionality. Will get a set number of videos starting at the supplied index
     if (videosPerPage && p_startIndex)
         playListURL += '&start-index=' + p_startIndex + '&max-results=' + videosPerPage;
 
+    // Return only HD videos
     if (hdOnly) {
         playListURL += '&hd';
     }
 
+    // https://developers.google.com/youtube/2.0/developers_guide_json
     $.getJSON(playListURL, function (data) {
         var videos = "";
         var count = 0;
+
+        if (showList)
+            videos += '<ul>';
+            
+        // $.each(index, value)
         $.each(data.feed.entry, function (i, item) {
             if (item != null) {
                 count++;
+
                 var newVideo = new Video(item);
-                videos += newVideo.videoBlock();
+
+                if (showList)
+                    videos += newVideo.listBlock();
+                else
+                    videos += newVideo.videoBlock();
             }
         });
+
+        if (showList)
+            videos += '</ul>';
 
         // Append next/prev buttons
         videos += '<div id="pager">';
@@ -97,16 +117,16 @@ function BuildPager(p_startIndex, p_videosOnPage) {
     var pages = Math.ceil(videosInCurrentPlaylist / videosPerPage);
 
     if (pages > 1) {
-        // Build Prev button
         if (p_startIndex != 1)
+            // Build Prev button
             builder += '<span class="button" start-index="' + (p_startIndex - videosPerPage) + '">Previous</span>';
 
-        var newActivePage = ActivePage(p_startIndex);
+        var activePage = ActivePage(p_startIndex);
         
         // Build numbered list
         for (var i = 1; i <= pages; i++) {
-            // Determine current page here?
-            if (i == newActivePage)
+            // Determines if the page should be marked as the 'active' page
+            if (i == activePage)
                 builder += '<span class="active">';
             else
                 builder += '<span start-index="' + GetStartIndex(i, p_startIndex) + '">';
@@ -124,9 +144,14 @@ function BuildPager(p_startIndex, p_videosOnPage) {
 }
 
 function ActivePage(p_startIndex) {
+    // ex (on first page, with 8 videos per page, num is 9)
     var num = parseInt(p_startIndex) + parseInt(videosPerPage);
     var count = 0;
+
     do {
+        // Subtract videosPerPage from num found earlier, and see if num is 1
+        // count keeps track of what page should be active
+        // When num is 1, count will be the number of the active page
         num -= videosPerPage;
         count++;
     } while (num != 1);
@@ -135,6 +160,8 @@ function ActivePage(p_startIndex) {
 }
 
 function GetStartIndex(p_page, p_startIndex) {
+    // Get the current page, subtract 1, and multiply times the number of videos per page
+    // Add 1 to get the start index of the videos on that page
     return ((p_page - 1) * videosPerPage) + 1;
 }
 
@@ -151,33 +178,51 @@ function Video(video) {
         this.author = video.author[0].name.$t;
         this.uploaded = video.published.$t;
         this.title = titleCharacters == 0 ? video.title.$t : video.title.$t.substring(0, titleCharacters);
+        this.published = video.published.$t.split('T')[0];
 
-        if (video.media$group.media$description != null) {
-            var desc = video.media$group.media$description.$t;
-            this.description = descriptionCharacters == 0 ? desc : desc.substring(0, descriptionCharacters);
-        }
-        else {
-            this.description = '';
-        }
-
-        if (video.yt$statistics != null) {
-            this.views = video.yt$statistics.viewCount;
-        }
-
-        if (video.yt$rating != null) {
-            this.likes = video.yt$rating.numLikes;
-            this.dislikes = video.yt$rating.numDislikes;
-
-            this.likeRatio = function () {
-                if (this.likes + this.dislikes > 0)
-                    return ((parseInt(this.likes) / (parseInt(this.likes) + parseInt(this.dislikes))).toFixed(2) * 100) - 1;
-                else
-                    return 0;
+        // Only care about the description if the user wants to show it
+        if (showDescription) {
+            // Ensure description is not null before assigning it
+            if (video.media$group.media$description != null) {
+                var desc = video.media$group.media$description.$t;
+                this.description = descriptionCharacters == 0 ? desc : desc.substring(0, descriptionCharacters);
             }
-            this.dislikeDisplay = this.dislikes == 1 ? this.dislikes + " dislike" : this.dislikes + " dislikes";
-            this.likeDisplay = this.likes == 1 ? this.likes + " like" : this.likes + " likes";
+            else {
+                this.description = '';
+            }
         }
 
+        // Only calculate views if the user wants views
+        if (showViews) {
+            // Ensure we have views
+            if (video.yt$statistics != null) {
+                this.views = video.yt$statistics.viewCount;
+            }
+            else {
+                this.views = 0;
+            }
+        }
+
+        // Only calculate ratings info if the user wants to show ratings
+        if (displayRatings) {
+            // Ensure we have ratings
+            if (video.yt$rating != null) {
+                this.likes = video.yt$rating.numLikes;
+                this.dislikes = video.yt$rating.numDislikes;
+
+                this.likeRatio = function () {
+                    if (this.likes + this.dislikes > 0)
+                        return ((parseInt(this.likes) / (parseInt(this.likes) + parseInt(this.dislikes))).toFixed(2) * 100) - 1;
+                    else
+                        return 0;
+                }
+                this.dislikeDisplay = this.dislikes == 1 ? this.dislikes + " dislike" : this.dislikes + " dislikes";
+                this.likeDisplay = this.likes == 1 ? this.likes + " like" : this.likes + " likes";
+            }
+        }
+
+        // Null check for the duration
+        // This is null for private videos
         if (video.media$group.yt$duration != null) {
             this.length = ConvertTime(video.media$group.yt$duration.seconds);
         }
@@ -185,6 +230,7 @@ function Video(video) {
             this.length = "0:00";
         }
 
+        // Add necessary above information to the video block
         this.videoBlock = function () {
             var block = '';
             block += '<div class="video">';
@@ -194,14 +240,18 @@ function Video(video) {
             block += '<span class="video_time">' + this.length + '</span>';
             block += '</div>';
             block += '<h3 class="yt_title">' + this.title + '</h3>';
+
             if (showViews) {
-                block += '<span>' + FormatViews(this.views) + ' | days ago</span>';
+                block += '<span>' + FormatViews(this.views) + '</span>';
             }
-            block += '<span>' + user + '</span>';
+
+            block += '<span>' + user + ' | ' + formatDate(this.published) + '</span>';
             block += '</a>';
+
             if (showDescription) {
                 block += '<span class="yt_desc">' + this.description + '</span>';
             }
+
             if (displayRatings && video.yt$rating != null && (this.likes > 0 || this.dislikes > 0)) {
                 block += '<div class="rating">';
                 block += '<span class="likes" style="width: ' + (this.likeRatio() - 1) + '%"></span>';
@@ -209,8 +259,21 @@ function Video(video) {
                 block += '<span style="width: 100%; height: 12px; clear: both;">' + this.likeDisplay + ", " + this.dislikeDisplay + '</span>';
                 block += '</div>';
             }
+
             block += '</div>';
             return block;
+        }
+
+        this.listBlock = function () {
+            var item = '';
+
+            item += '<li>';
+            item += '<a href="' + this.embedURL + '?autoplay=1" class="fancy_video">';
+            item += '<h3 class="yt_title">' + this.title + '</h3>';
+            item += '</a>';
+            item += '</li>';
+
+            return item;
         }
     }
 }
@@ -288,6 +351,27 @@ function addCommas(nStr)
     return nStr;
 }
 
+function formatDate(time) {
+    var today = new Date();
+    var videoDate = new Date(time);
+    var years;
+    var months;
+    var days;
+    var retval;
+
+    years = today.getYear() - videoDate.getYear();
+    months = today.getMonth() - videoDate.getMonth();
+    days = today.getDay() - videoDate.getDay();
+
+    if (years > 0)
+        retval = years + ' years';
+    else if (months > 0)
+        retval = months + ' months';
+    else if (days > 0)
+        retval = days + ' days';
+
+    return retval + ' ago';
+}
 
 (function ($) {
     $.fn.YouTube = function (options) {
@@ -301,25 +385,30 @@ function addCommas(nStr)
             descriptionCharacters: 200,
             views: true,
             hdOnly: false,
-            orderBy: 'viewCount' // Add orderBy for videos and playlists https://developers.google.com/youtube/2.0/reference#orderbysp ??
+            list: false
         };
-        var options = $.extend(defaults, options);
+        var settings = $.extend(defaults, options);
 
         return this.each(function () {
             // Set Global Variables
-			displayRatings = options.ratings;
-            numberOfPlaylists = options.numberOfPlaylists;
-            videosPerPage = options.videosPerPage;
-            showDescription = options.description;
-            showViews = options.views;
-            user = options.username;
-            if (options.titleCharacters != '') {
-                titleCharacters = options.titleCharacters;
+			displayRatings = settings.ratings;
+            numberOfPlaylists = settings.numberOfPlaylists;
+            videosPerPage = settings.videosPerPage;
+            showDescription = settings.description;
+            showViews = settings.views;
+            user = settings.username;
+
+            if (settings.titleCharacters != '') {
+                titleCharacters = settings.titleCharacters;
             }
-            if (options.descriptionCharacters != '') {
-                descriptionCharacters = options.descriptionCharacters;
+
+            if (settings.descriptionCharacters != '') {
+                descriptionCharacters = settings.descriptionCharacters;
             }
-            hdOnly = options.hdOnly;
+
+            hdOnly = settings.hdOnly;
+
+            showList = settings.list;
 
             // Get the playlists
             GetPlaylists(user);
